363ac7d5e8fe7b6db582368c540f444319729177
TokumeiKibou01 <snowsansub0113@gmail.com>
2025-11-24 10:35:00 +0900

    【追加】 入力を受け取るように

--- C-Sharp_Weather_API_Library/Program.cs
+++ C-Sharp_Weather_API_Library/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            string? line = Console.ReadLine(); //入力を受け取る＆null許容
+            if (line != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
+
