11922faa7f18acda1f1d23265accf6345144172d
TokumeiKibou01 <snowsansub0113@gmail.com>
2025-11-24 11:04:20 +0900

    【追加】 コマンドが実行できるように

---
+++ C-Sharp_Weather_API_Library/CommandBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Weather_API_Library
+{
+    internal interface CommandBase
+    {
+        void runCommand(string command);
+    }
+}
--- C-Sharp_Weather_API_Library/Program.cs
+++ C-Sharp_Weather_API_Library/Program.cs
@@ -1,17 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using C_Sharp_Weather_API_Library;
+
 class Program
 {
+
+    private static Dictionary<string, CommandBase> cmd_dictionary = new Dictionary<string, CommandBase>();
+
     public static void Main(string[] args)
     {
+        initLibrary();
         while (true)
         {
             string? line = Console.ReadLine(); //入力を受け取る＆null許容
             if (line != null)
             {
-                Console.WriteLine(line);
+                if (cmd_dictionary.ContainsKey(line))
+                {
+                    CommandBase cmd = cmd_dictionary[line];
+                    cmd.runCommand(line);
+                }
+                else
+                {
+                    Console.WriteLine("そのコマンドは見つかりませんでした。");
+                }
+                Console.Write(">");
+            }
+            else
+            {
+                Console.Write(">");
             }
         }
+    }
+
+    public static void initLibrary()
+    {
+        cmd_dictionary.Add("info", new InfoCommand());
+
+        Console.Write(">");
     }
 }
 
