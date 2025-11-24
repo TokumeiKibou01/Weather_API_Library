3bcbee2236d72853a6210d65dd36e9648a38e9ac
TokumeiKibou01 <snowsansub0113@gmail.com>
2025-11-24 13:54:31 +0900

    【追加】 天気が取得できるように!

---
+++ C-Sharp_Weather_API_Library/WeatherCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static C_Sharp_Weather_API_Library.OpenWeatherAPI;
+using static C_Sharp_Weather_API_Library.OpenWeatherAPI.WeatherList;
+
+namespace C_Sharp_Weather_API_Library
+{
+    internal class WeatherCommand : CommandBase
+    {
+
+        void CommandBase.runCommand(string command)
+        {
+            if (command.Equals("get_weather"))
+            {
+                OpenWeatherAPI openWeatherAPI = OpenWeatherAPI.GetInstanceAsync().Result;
+                WeatherList weatherList = openWeatherAPI.GetWeatherList[0];
+                Weather weather = weatherList.GetWeather[0];
+                WeatherMain weatherMain = weatherList.GetWeatherMain;
+
+                Console.WriteLine("天気：" + weather.GetDescription + "\n" +
+                    "最高気温：" + weatherMain.GetTempMax + "\n" +
+                    "最低気温： " + weatherMain.GetTempMin + "\n");
+            }
+        }
+    }
+}
